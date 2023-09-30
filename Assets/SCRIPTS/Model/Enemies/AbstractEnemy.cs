27c3f0a360466a81194ce8514ebe6e5c504d81c2
Umbrason <45980080+Umbrason@users.noreply.github.com>
2023-09-30 02:45:25 +0200

    Add enemy health properties

--- Assets/SCRIPTS/Model/Enemies/AbstractEnemy.cs
+++ Assets/SCRIPTS/Model/Enemies/AbstractEnemy.cs
@@ -1,5 +1,7 @@
 public abstract class AbstractEnemy
 {
     public int Cooldown { get; set; }
+    public int Health { get; set; }
+    public int MaxHealth { get; set; }
     public abstract void Act();
 }
