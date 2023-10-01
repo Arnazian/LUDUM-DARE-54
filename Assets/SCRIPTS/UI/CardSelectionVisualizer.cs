29ec5ff442c208d7c372977927e16e6fa2572e0a
Umbrason <45980080+Umbrason@users.noreply.github.com>
2023-10-01 05:11:38 +0200

    Fix CardTarget Selection

--- Assets/SCRIPTS/UI/CardSelectionVisualizer.cs
+++ Assets/SCRIPTS/UI/CardSelectionVisualizer.cs
@@ -60,10 +60,10 @@ public class CardSelectionVisualizer : MonoBehaviour
         var delta = (to - from) / dotCount;
         for (int i = 0; i < dotCount; i++)
         {
-            var pos = delta + from;
+            var pos = delta * (i + .5f) + from;
             var cross = Vector2.Perpendicular(delta);
             if (cross.y < 0) cross = -cross;
-            //pos += cross * lineCurvature.Evaluate(i / (float)dotCount);
+            pos += cross * lineCurvature.Evaluate(i / (float)dotCount) * len;
             Dots[i].position = new(pos.x, pos.y, Dots[i].position.z);
         }
     }
@@ -78,10 +78,15 @@ public class CardSelectionVisualizer : MonoBehaviour
     void End(object _)
     {
         Crosshair.gameObject.SetActive(false);
+        while (Dots.Count > 0)
+        {
+            Destroy(Dots[0].gameObject);
+            Dots.RemoveAt(0);
+        }
         Cursor.visible = true;
     }
 
-    void OnEnter(Vector2 pos) => OverridePosition = pos;
+    void OnEnter(Vector2 p) => OverridePosition = RootCanvas.worldCamera.ScreenToWorldPoint(new(p.x, p.y, RootCanvas.planeDistance));
     void OnExit() => OverridePosition = null;
 
 }
--- Assets/SCRIPTS/View/Card/CardComponent.cs
+++ Assets/SCRIPTS/View/Card/CardComponent.cs
@@ -24,7 +24,7 @@ public class CardComponent : MonoBehaviour, IDragHandler, IBeginDragHandler, IEn
     public DrivenSpring GlowEffectSpring { get; private set; }
 
     Canvas cached_RootCanvas;
-    Canvas RootCanvas => cached_RootCanvas ??= this.GetRootComponent<Canvas>();
+    public Canvas RootCanvas => cached_RootCanvas ??= this.GetRootComponent<Canvas>();
 
     CanvasGroup cached_RootCanvasGroup;
     CanvasGroup RootCanvasGroup => cached_RootCanvasGroup ??= this.GetRootComponent<CanvasGroup>();
