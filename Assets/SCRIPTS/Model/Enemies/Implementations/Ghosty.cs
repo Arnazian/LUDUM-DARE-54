499da6899477a7bd209764df5a0e8793ba20ee78
Umbrason <45980080+Umbrason@users.noreply.github.com>
2023-10-02 01:56:27 +0200

    Create Enemy classes, small fixes

---
+++ Assets/SCRIPTS/Model/Enemies/Implementations/Ghosty.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghosty : AbstractEnemy
+{
+    protected override CappedInt ActCooldown { get; set; } = new(2, 2);
+    protected override CappedInt Health { get; set; } = new(10, 10);
+    public override void Act()
+    {
+        DealDamage(5);
+    }
+}
