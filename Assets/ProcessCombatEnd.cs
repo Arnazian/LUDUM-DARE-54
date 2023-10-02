cda2a321fadbb173fde647e2768da00c760fedb2
Umbrason <45980080+Umbrason@users.noreply.github.com>
2023-10-02 14:18:43 +0200

    fix script ordering

--- Assets/ProcessCombatEnd.cs
+++ Assets/ProcessCombatEnd.cs
@@ -14,9 +14,12 @@ public class ProcessCombatEnd : MonoBehaviour
     {
         if (e.Type != CombatEvent.EventType.Killed) return;
         if (e.Target == GameSession.Player) GameSession.GameState = GameSession.State.GAME_OVER;
-        else if (Combat.Active.Enemies.Count == 0) GameSession.GameState = GameSession.State.LOOT;
-        GameSession.OfferedCard = CardGroups.GetRandom(Combat.Active.difficulty);
-        GameSession.ActiveCombat = null;
+        else if (Combat.Active.Enemies.Count == 0)
+        {
+            GameSession.OfferedCard = CardGroups.GetRandom(Combat.Active.difficulty);
+            GameSession.ActiveCombat = null;
+            GameSession.GameState = GameSession.State.LOOT;
+        }
     }
     void OnDestroyed()
     {
