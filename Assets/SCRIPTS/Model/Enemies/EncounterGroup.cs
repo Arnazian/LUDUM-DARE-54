0692a9c4cebeaa56371251417f12b4f7f59400f1
Umbrason <45980080+Umbrason@users.noreply.github.com>
2023-10-02 23:36:22 +0200

    Final? version

--- Assets/SCRIPTS/Model/Enemies/EncounterGroup.cs
+++ Assets/SCRIPTS/Model/Enemies/EncounterGroup.cs
@@ -12,7 +12,6 @@ public static class EncounterGroups
         {Difficulty.Medium, Medium},
         {Difficulty.Hard, Hard},
         {Difficulty.Boss, Boss},
-
     };
 
     public static List<List<AbstractEnemy>> Easy => new()
@@ -20,6 +19,7 @@ public static class EncounterGroups
         new() { new BabyBag(), new Ghosty()},
         new() { new BabyBag(), new Pumpkin()},
         new() { new Pumpkin(), new Ghosty()},
+        new() { new MischievousPumpkin()},
 
         new() { new Pumpkin(), new Pumpkin()},
         new() { new Ghosty(), new Ghosty()},
@@ -40,14 +40,24 @@ public static class EncounterGroups
         new() { new Pumpkin(), new Pumpkin(), new Pumpkin()},
         new() { new BabyBag(), new BabyBag(), new BabyBag()},
 
+        new() { new Pumpkin(), new VengefulPumpkin()},
+        new() { new Pumpkin(), new MischievousPumpkin()},
+
+        new() { new BabyBag(), new BabyBagJoker(), new Ghosty()},
+        new() { new BabyBag(), new BabyBagGrumpy(), new Ghosty()},
+        new() { new BabyBag(), new BabyBagQueen(), new Ghosty()},
+
     };
 
     public static List<List<AbstractEnemy>> Hard => new()
     {
-         new() { new WitchSkull()},
-         new() { new BabyBag(), new BabyBag(), new Pumpkin(), new Pumpkin()},
-         new() { new BabyBag(), new BabyBag(), new Ghosty(), new Ghosty()},
+         new() { new BabyBag(), new BabyBagJoker(), new VengefulPumpkin(), new Pumpkin()},
+         new() { new BabyBag(), new BabyBagGrumpy(), new Ghosty(), new Ghosty()},
          new() { new Pumpkin(), new Pumpkin(), new Ghosty(), new Ghosty()},
+
+
+         new() { new Pumpkin(), new VengefulPumpkin(), new MischievousPumpkin()},
+         new() { new BabyBag(), new BabyBagGrumpy(), new BabyBagJoker(), new BabyBagQueen()}
 
     };
 
