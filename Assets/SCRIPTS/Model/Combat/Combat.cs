fd8ab72fc6d5ab5d0c527428e77dc77b0e43bd74
Umbrason <45980080+Umbrason@users.noreply.github.com>
2023-10-02 13:45:19 +0200

    fix skip turn

--- Assets/SCRIPTS/Model/Combat/Combat.cs
+++ Assets/SCRIPTS/Model/Combat/Combat.cs
@@ -13,22 +13,22 @@ public class Combat
     public IReadOnlyList<CombatEvent> ImmutableEventLogHistory => EventLogHistory;
     private readonly Queue<CombatEvent> EventLog = new();
     public void ConsumeCombatEvent(CombatEvent e)
-    {        
+    {
         if (EventLog.Count == 0) return; //queue empty
         if (EventLog.Peek() == e) EventLog.Dequeue();
-        EventLogHistory.Add(e);        
+        EventLogHistory.Add(e);
         if (EventLog.Count == 0) return; //queue empty
         e = EventLog.Peek();
         _OnEventLogChanged?.Invoke(e);
-        if(e.users == 0 && EventLog.Count > 0) ConsumeCombatEvent(e);        
+        if (e.users == 0 && EventLog.Count > 0) ConsumeCombatEvent(e);
     }
 
     public void PushCombatEvent(CombatEvent e)
     {
-        EventLog.Enqueue(e);        
+        EventLog.Enqueue(e);
         if (EventLog.Count > 1) return; //'e' not visible yet
         _OnEventLogChanged?.Invoke(EventLog.Peek());
-        if(e.users == 0) ConsumeCombatEvent(e);
+        if (e.users == 0) ConsumeCombatEvent(e);
     }
 
     private static event Action<CombatEvent> _OnEventLogChanged;
@@ -50,6 +50,12 @@ public class Combat
 
     public void Pass()
     {
+        foreach (var c in Player.Cards.Where(c => c != null))
+            c.Cooldown.Value--;
+        EnemyTurn();
+    }
+    private void EnemyTurn()
+    {
         PushCombatEvent(CombatEvent.TurnEnded(Player));
         IStatusEffectTarget.OnEndTurn(Player);
         ProcessEnemies();
@@ -69,7 +75,7 @@ public class Combat
         PushCombatEvent(CombatEvent.TakenAction(Player));
 
         //End Turn
-        Pass();
+        EnemyTurn();
     }
 
     public void ProcessEnemies()
@@ -77,7 +83,7 @@ public class Combat
         foreach (var enemy in Enemies.ToList())
         {
             PushCombatEvent(CombatEvent.TurnStarted(enemy));
-            enemy.DoTurn();            
+            enemy.DoTurn();
             PushCombatEvent(CombatEvent.TurnEnded(enemy));
         }
         if (Enemies.Count == 0) //won combat
